7b8debd7ad34c6188a01f326dde6cc3ff13db7ac
shyam <shyamgodavari7@gmail.com>
2024-03-21 18:08:42 +0530

    ini

--- src/thTime/Hi.java
+++ src/thTime/Hi.java
@@ -4,7 +4,9 @@ public class Hi {
 	public static void main(String[] args) {
 		System.out.println("hiiiiiii");
 		System.out.println("earth is flat");
-		System.out.println("no earth is round");
+		System.out.println("who kows");
+		
+		
 	}
 	
 
